57db9742a49adb7b03ce904b67325f961c2ef7f1
huweiliang <huweiliang@kuaiqiangche.com>
2016-04-19 19:37:45 +0800

    update

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,18 @@
 
 module.exports = function(app) {
     app.get('/', function(req, res){
-    	res.render('index',{
-            
-        });
+    	res.render('index');
     });
-}
+
+    app.get('/create_page', function(req, res){
+        res.render('creat_page');
+    });
+
+    app.get('/edit_page', function(req, res){
+        res.render('edit_page');
+    });
+
+    app.get('/view', function(req, res){
+        res.render('view');
+    });
+};
